69411b74f1707350ed6ef0fedb4542c6f5d7ee05
Christian Döring <doering.christian.2001@gmx.de>
2021-10-29 22:52:12 +0200

    Added convolution operation and copy operators.

--- nmath.h
+++ nmath.h
@@ -36,6 +36,10 @@
 #define NM_SIZE size_t
 #endif //NM_SIZE
 
+#ifndef NM_INT
+#define NM_INT int
+#endif //NM_INT
+
 #ifndef NM_MALLOC
 #include <stdlib.h>
 #define NM_MALLOC(_size) malloc(_size)
@@ -106,6 +110,13 @@ static NM_INLINE NM_FLOAT *matnm_new(NM_SIZE n, NM_SIZE m){
 
 static NM_INLINE void matnm_free(NM_FLOAT *dst){
     NM_FREE(dst);
+}
+
+static NM_INLINE NM_FLOAT *matnm_copy(NM_FLOAT *dst, const NM_FLOAT *src, NM_SIZE n, NM_SIZE m){
+    NM_SIZE i;
+    for(i = 0;i < n * m;i++)
+        dst[i] = src[i];
+    return dst;
 }
 
 static NM_INLINE NM_FLOAT *matnm_foreach_cb(NM_FLOAT *dst, const NM_FLOAT *src, void *data, NM_FLOAT (*cb)(NM_FLOAT src, void *data), NM_SIZE n, NM_SIZE m){
@@ -263,6 +274,112 @@ static NM_INLINE NM_FLOAT *matnm_multt(NM_FLOAT *dst, const NM_FLOAT *src1, cons
     return dst;
 }
 
+static NM_INLINE NM_FLOAT *matnm_mult_at(NM_FLOAT *dst, const NM_FLOAT *src1, NM_FLOAT *src2, NM_SIZE n1, NM_SIZE m1, NM_SIZE n2, NM_SIZE m2, NM_INT x, NM_INT y){
+    NM_SIZE i, j;
+    for(i = 0;i < n2;i++){
+        for(j = 0;j < m2;j++){
+            *matnm_at(dst, i, j, n2) = matnm_get(src1, i + x, j + y, n1) * matnm_get(src2, i, j, n2);
+        }
+    }
+    return dst;
+}
+
+static NM_INLINE NM_FLOAT matnm_conv_at(const NM_FLOAT *src1, NM_FLOAT *src2, NM_SIZE n1, NM_SIZE m1, NM_SIZE n2, NM_SIZE m2, NM_INT x, NM_INT y){
+    NM_SIZE i, j;
+    NM_FLOAT dst = 0.0;
+    for(i = 0;i < n2;i++){
+        for(j = 0;j < m2;j++){
+            dst += matnm_get(src1, i + x, j + y, n1) * matnm_get(src2, i, j, n2);
+        }
+    }
+    return dst;
+}
+
+/*
+ * =======================
+ * Matrix N x N          :
+ * =======================
+ *
+ * N x N matrix operations can be generalized to N x M matrix operations.
+ */
+
+static NM_INLINE NM_FLOAT *matn_new(NM_SIZE n, NM_SIZE m){
+    return (NM_FLOAT *)NM_MALLOC(sizeof(NM_FLOAT) * n * m);
+}
+
+static NM_INLINE void matn_free(NM_FLOAT *dst){
+    NM_FREE(dst);
+}
+
+static NM_INLINE NM_FLOAT *matn_copy(NM_FLOAT *dst, const NM_FLOAT *src, NM_SIZE n){
+    return matnm_copy(dst, src, n, n);
+}
+
+static NM_INLINE NM_FLOAT *matn_foreach_cb(NM_FLOAT *dst, const NM_FLOAT *src, void *data, NM_FLOAT (*cb)(NM_FLOAT src, void *data), NM_SIZE n, NM_SIZE m){
+    return matnm_foreach_cb(dst, src, data, cb, n, n);
+}
+
+static NM_INLINE NM_FLOAT *matn_one(NM_FLOAT *dst, NM_SIZE n){
+    return matnm_one(dst, n, n);
+}
+
+static NM_INLINE NM_FLOAT *matn_zero(NM_FLOAT *dst, NM_SIZE n){
+    return matnm_zero(dst, n, n);
+}
+
+static NM_INLINE NM_FLOAT *matn_at(NM_FLOAT *src, NM_SIZE row, NM_SIZE col, NM_SIZE n){
+    return matnm_at(src, col, row, n);
+}
+
+static NM_INLINE NM_FLOAT *matn_att(NM_FLOAT *src, NM_SIZE row, NM_SIZE col, NM_SIZE n){
+    return matnm_att(src, col, row, n);
+}
+
+const static NM_INLINE NM_FLOAT *matn_atc(const NM_FLOAT *src, NM_SIZE row, NM_SIZE col, NM_SIZE n){
+    return matnm_atc(src, col, row, n);
+}
+
+const static NM_INLINE NM_FLOAT *matn_attc(const NM_FLOAT *src, NM_SIZE row, NM_SIZE col, NM_SIZE n){
+    return matnm_attc(src, col, row, n);
+}
+
+
+static NM_INLINE NM_FLOAT matn_get(const NM_FLOAT *src, NM_SIZE row, NM_SIZE col, NM_SIZE n){
+    return matnm_get(src, col, row, n);
+}
+
+static NM_INLINE NM_FLOAT matn_gett(const NM_FLOAT *src, NM_SIZE row, NM_SIZE col, NM_SIZE n){
+    return matnm_gett(src, col, row, n);
+}
+
+static NM_INLINE NM_FLOAT *matn_t(NM_FLOAT *dst, const NM_FLOAT *src, NM_SIZE n){
+    return matnm_t(dst, src, n, n);
+}
+
+static NM_INLINE NM_FLOAT *matn_add(NM_FLOAT *dst, const NM_FLOAT *src1, const NM_FLOAT *src2, NM_SIZE n){
+    return matnm_add(dst, src1, src2, n, n);
+}
+
+static NM_INLINE NM_FLOAT *matn_sub(NM_FLOAT *dst, const NM_FLOAT *src1, const NM_FLOAT *src2, NM_SIZE n){
+    return matnm_sub(dst, src1, src2, n, n);
+}
+
+static NM_INLINE NM_FLOAT *matn_scale(NM_FLOAT *dst, const NM_FLOAT *src, NM_FLOAT s, NM_FLOAT n){
+    return matnm_scale(dst, src, s, n, n);
+}
+
+static NM_INLINE NM_FLOAT *matn_mult(NM_FLOAT *dst, const NM_FLOAT *src1, const NM_FLOAT *src2, NM_SIZE n){
+    return matnm_mult(dst, src1, src2, n, n, n);
+}
+
+static NM_INLINE NM_FLOAT *matn_multt(NM_FLOAT *dst, const NM_FLOAT *src1, const NM_FLOAT *src2_t, NM_SIZE n){
+    return matnm_multt(dst, src1, src2_t, n, n, n);
+}
+
+static NM_INLINE NM_FLOAT *matn_mult_vecn(NM_FLOAT *dst_v, const NM_FLOAT *src1_m, const NM_FLOAT *src2_v, NM_SIZE n){
+    return matnm_mult(dst_v, src1_m, src2_v, n, n, 1);
+}
+
 /*
  * =======================
  * Vector N x 1 or 1 X N :
@@ -277,6 +394,10 @@ static NM_INLINE NM_FLOAT *vecn_new(NM_SIZE n){
 
 static NM_INLINE void vecn_free(NM_FLOAT *dst){
     NM_FREE(dst);
+}
+
+static NM_INLINE NM_FLOAT *vecn_copy(NM_FLOAT *dst, const NM_FLOAT *src, NM_SIZE n){
+    return matnm_copy(dst, src, n, 1);
 }
 
 static NM_INLINE NM_FLOAT *vecn_foreach_cb(NM_FLOAT *dst, const NM_FLOAT *src, void *data, NM_FLOAT (*cb)(NM_FLOAT src, void *data), NM_SIZE n){
@@ -361,85 +482,5 @@ static NM_INLINE NM_FLOAT *vecn_floor(NM_FLOAT *dst, const NM_FLOAT *src, NM_SIZ
     return dst;
 }
 
-/*
- * =======================
- * Matrix N x N          :
- * =======================
- *
- * N x N matrix operations can be generalized to N x M matrix operations.
- */
-
-static NM_INLINE NM_FLOAT *matn_new(NM_SIZE n, NM_SIZE m){
-    return (NM_FLOAT *)NM_MALLOC(sizeof(NM_FLOAT) * n * m);
-}
-
-static NM_INLINE void matn_free(NM_FLOAT *dst){
-    NM_FREE(dst);
-}
-
-static NM_INLINE NM_FLOAT *matn_foreach_cb(NM_FLOAT *dst, const NM_FLOAT *src, void *data, NM_FLOAT (*cb)(NM_FLOAT src, void *data), NM_SIZE n, NM_SIZE m){
-    return matnm_foreach_cb(dst, src, data, cb, n, n);
-}
-
-static NM_INLINE NM_FLOAT *matn_one(NM_FLOAT *dst, NM_SIZE n){
-    return matnm_one(dst, n, n);
-}
-
-static NM_INLINE NM_FLOAT *matn_zero(NM_FLOAT *dst, NM_SIZE n){
-    return matnm_zero(dst, n, n);
-}
-
-static NM_INLINE NM_FLOAT *matn_at(NM_FLOAT *src, NM_SIZE row, NM_SIZE col, NM_SIZE n){
-    return matnm_at(src, col, row, n);
-}
-
-static NM_INLINE NM_FLOAT *matn_att(NM_FLOAT *src, NM_SIZE row, NM_SIZE col, NM_SIZE n){
-    return matnm_att(src, col, row, n);
-}
-
-const static NM_INLINE NM_FLOAT *matn_atc(const NM_FLOAT *src, NM_SIZE row, NM_SIZE col, NM_SIZE n){
-    return matnm_atc(src, col, row, n);
-}
-
-const static NM_INLINE NM_FLOAT *matn_attc(const NM_FLOAT *src, NM_SIZE row, NM_SIZE col, NM_SIZE n){
-    return matnm_attc(src, col, row, n);
-}
-
-
-static NM_INLINE NM_FLOAT matn_get(const NM_FLOAT *src, NM_SIZE row, NM_SIZE col, NM_SIZE n){
-    return matnm_get(src, col, row, n);
-}
-
-static NM_INLINE NM_FLOAT matn_gett(const NM_FLOAT *src, NM_SIZE row, NM_SIZE col, NM_SIZE n){
-    return matnm_gett(src, col, row, n);
-}
-
-static NM_INLINE NM_FLOAT *matn_t(NM_FLOAT *dst, const NM_FLOAT *src, NM_SIZE n){
-    return matnm_t(dst, src, n, n);
-}
-
-static NM_INLINE NM_FLOAT *matn_add(NM_FLOAT *dst, const NM_FLOAT *src1, const NM_FLOAT *src2, NM_SIZE n){
-    return matnm_add(dst, src1, src2, n, n);
-}
-
-static NM_INLINE NM_FLOAT *matn_sub(NM_FLOAT *dst, const NM_FLOAT *src1, const NM_FLOAT *src2, NM_SIZE n){
-    return matnm_sub(dst, src1, src2, n, n);
-}
-
-static NM_INLINE NM_FLOAT *matn_scale(NM_FLOAT *dst, const NM_FLOAT *src, NM_FLOAT s, NM_FLOAT n){
-    return matnm_scale(dst, src, s, n, n);
-}
-
-static NM_INLINE NM_FLOAT *matn_mult(NM_FLOAT *dst, const NM_FLOAT *src1, const NM_FLOAT *src2, NM_SIZE n){
-    return matnm_mult(dst, src1, src2, n, n, n);
-}
-
-static NM_INLINE NM_FLOAT *matn_multt(NM_FLOAT *dst, const NM_FLOAT *src1, const NM_FLOAT *src2_t, NM_SIZE n){
-    return matnm_multt(dst, src1, src2_t, n, n, n);
-}
-
-static NM_INLINE NM_FLOAT *matn_mult_vecn(NM_FLOAT *dst_v, const NM_FLOAT *src1_m, const NM_FLOAT *src2_v, NM_SIZE n){
-    return matnm_mult(dst_v, src1_m, src2_v, n, n, 1);
-}
 
 #endif //NMATH_H
